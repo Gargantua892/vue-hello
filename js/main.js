2403637338cf8d19bca437bc1ef2d56d5f3fcf00
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-05-18 16:06:11 +0200

    fine esercizio con bonus

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "Eccoci live"
+            message: "Ready for the summer?",
+            image: 'https://images.unsplash.com/photo-1682289570915-5d6e41ade8bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80'
         }
     },
-}).mount("#hello");
+}).mount("#app");
